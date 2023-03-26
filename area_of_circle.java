814af5031e41c6cfb3581db69f6a229e89ed1a35
bhavishya <bhavisachdeva3102@gmail.com>
2023-03-26 18:09:32 +0530

    first commit

---
+++ area_of_circle.java
@@ -0,0 +1,13 @@
+public class area_of_circle {
+    
+        float r=2.6f;
+        double ar;
+        public static void main(String args[]){
+            area_of_circle s=new  area_of_circle();
+            s.ar=3.14*s.r*s.r;
+            System.out.println("Area of Circle"+s.ar);
+        
+    
+    }
+    
+}
