e6dc9ad3f5cb753d8c02e387d77c42faadfddd90
bhavishya <121548550+bhavishya3102@users.noreply.github.com>
2023-03-26 18:13:55 +0530

    update area of rect instantiate file

--- area_of_rect_inst.java
+++ area_of_rect_inst.java
@@ -5,7 +5,7 @@ public class area_of_rect_inst {
         float l=2,b=4;
     double ar;
         ar=l*b;
-        System.out.println("Area of Circle"+ar);
+        System.out.println("Area of Rectangle"+ar);
     
 }
     
